58b102b6eb69bed350df9ff59e1cd3bb864c7102
TOMAS <tapr@uc.cl>
2024-11-17 18:40:31 -0300

    se agregó el calculo de los factores

--- E2/ansalisis_sens.py
+++ E2/ansalisis_sens.py
@@ -28,10 +28,13 @@ def simular(umbrales, cantidad_x_umbral, tiempo):
         lista_umbrales = list()
         lista_kms = list()
         lista_tons = list()
+        factores_toneladas = list()
+        factores_kilometros = list()
         for i in range (cantidad_x_umbral):
             print(f"Simulación número {i}")
             simulacion = Simulacion(tiempo, umbral)
             simulacion.inicio_politica_umbral_Cox()
+            #simulacion.inicio_politica_reactiva()
 
             fallas_totales = simulacion.camion.CFallas 
             fallas_programadas = simulacion.camion.CFallaP
@@ -48,6 +51,8 @@ def simular(umbrales, cantidad_x_umbral, tiempo):
             lista_umbrales.append(umbral)
             lista_kms.append(simulacion.camion.CKT)
             lista_tons.append(simulacion.camion.CTT)
+            factores_toneladas.append((simulacion.camion.CTT/simulacion.camion.TOperacion)/simulacion.camion.TReparacion)
+            factores_kilometros.append((simulacion.camion.CKT/simulacion.camion.TOperacion)/simulacion.camion.TReparacion)
             
         print(f"Fallas totales promedio: {sum(Ftotales)/len(Ftotales)}")
         print(f"Fallas programadas promedio: {sum(Fprogramadas)/len(Fprogramadas)}")
@@ -56,6 +61,8 @@ def simular(umbrales, cantidad_x_umbral, tiempo):
         print(f"Tiempo de operacion promedio: {sum(tiempo_op) / len(tiempo_op)}")
         print(f"Tiempo sin operar promedio: {sum(tiempo_sin) / len(tiempo_sin)}")
         print(f"Tiempo de Operacion/Tiempo de Reparacion: {sum(tiempo_op)/sum(tiempo_rep)}")  
+        print(f"Factor Toneladas: {sum(factores_toneladas)/len(factores_toneladas)}")
+        print(f"Factor Kilometros: {sum(factores_kilometros)/len(factores_kilometros)}")
         print(f"\n\nTiempo total de ejecución para {cantidad_x_umbral} repeticiones de {tiempo/8640} año/s es: {time.time() - start} segundos")
         var0 = lista_n_simulaciones
         var1 = (Ftotales)
@@ -68,6 +75,8 @@ def simular(umbrales, cantidad_x_umbral, tiempo):
         var8 = tiempo_rep
         var9 = lista_kms
         var10 = lista_tons
+        var11 = factores_toneladas
+        var12 = factores_kilometros
 
         data = {
             "Umbral": lista_umbrales,
@@ -81,13 +90,15 @@ def simular(umbrales, cantidad_x_umbral, tiempo):
             "Tiempo de Operacion/Tiempo de Reparacion": var7,
             "Tiempo_Reparacion": var8,
             "Kilometros totales": var9,
-            "Toneladas totales": var10
+            "Toneladas totales": var10,
+            "Factor Toneladas": var11,
+            "Factor Kilometros": var12
         }
                     
         df_new = pd.DataFrame(data)
 
         # Nombre del archivo CSV donde se guardarán los datos
-        csv_file = "registro_variables.csv"
+        csv_file = "registro_variables_new_2.csv"
 
         try:
             # Intentar leer el archivo CSV existente
@@ -103,10 +114,12 @@ def simular(umbrales, cantidad_x_umbral, tiempo):
         # Guardar el DataFrame en el archivo CSV
         df_final.to_csv(csv_file, index=False, encoding="utf-8")
 
-        print("Valores guardados en 'registro_variables.csv'.")
+        print("Valores guardados en 'registro_variables_new_2.csv'.")
 
 valores_simulados = [
     0.358
 ]
 
-simular(valores_simulados, 10, 8760)
+#SI QUEREMOS POLÍTICA REACTIVA, QUE ES UN CASO BASE, PONER UMBRAL 0
+
+simular(valores_simulados, 5, 8760)
